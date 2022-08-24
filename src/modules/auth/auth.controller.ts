1c9dd7d8289a8c57a5c26dfcef8a0ae8f57ea682
jovanamatk <mat.jovana@gmail.com>
2022-08-24 23:46:17 +0200

    fix: return HTTP error 400 instead of 500

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -15,7 +15,7 @@ const authController = (app) => {
       });
       return res.status(201).json({ id, email });
     } catch (error) {
-      return res.status(500).json({ error: error.message });
+      return res.status(400).json({ error: error.message });
     }
   });
 };
